b6ca1ef1fe697b27366312c932ab39dac0bb3b69
Zemelua <zemelua1107@gmail.com>
2025-06-22 22:43:54 +0900

    ❯❯ 🐣 ❯❯ ￤ Initial commit

---
+++ tsup.config.ts
@@ -0,0 +1,11 @@
+import { defineConfig } from 'tsup';
+
+export default defineConfig({
+	entry: ['src/index.ts'],
+	format: ['esm', 'cjs'],
+	dts: true,
+	outDir: 'dist',
+	clean: true,
+	sourcemap: true,
+	target: 'esnext',
+});
