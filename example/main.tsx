8d932e59c8f49c482e7938ae62dc3c03cc87f9d1
Zemelua <zemelua1107@gmail.com>
2025-06-23 18:46:12 +0900

    ₍₍⁽⁽🎈₎₎⁾⁾ ￤ example コードを更新

--- example/main.tsx
+++ example/main.tsx
@@ -1,9 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { H } from "~/components/H";
 import { SectionSContent } from "~/components/utils";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
 	<React.StrictMode>
-		<SectionSContent />
+		<SectionSContent>
+			<H>Hello, World! This is the level 1 heading.</H>
+
+			<SectionSContent>
+				<H>Hello, World! This is the level 2 heading.</H>
+			</SectionSContent>
+		</SectionSContent>
 	</React.StrictMode>
 );
