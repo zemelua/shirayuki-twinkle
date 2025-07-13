6fd54e6813cda053774d0e75b283b99b9510df73
Zemelua <zemelua1107@gmail.com>
2025-07-14 01:14:16 +0900

    ₍₍⁽⁽🐛₎₎⁾⁾ ￤ アウトラインレベルが1より小さいまたは6より大きいときの挙動を修正

--- src/components/H.tsx
+++ src/components/H.tsx
@@ -4,7 +4,7 @@ import { useOutlineLevel } from "~/hooks/useOutlineLevel";
 export function H({ children, ...props }: { children?: React.ReactNode; } & JSX.IntrinsicElements["h1" | "h2" | "h3" | "h4" | "h5" | "h6"]): JSX.Element {
 	const level = useOutlineLevel();
 
-	const Tag = `h${level}` as "h1" | "h2" | "h3" | "h4" | "h5" | "h6";
-	
+	const Tag = `h${Math.max(1, Math.min(6, level))}` as "h1" | "h2" | "h3" | "h4" | "h5" | "h6";
+
 	return <Tag {...props}>{children}</Tag>;
 }
